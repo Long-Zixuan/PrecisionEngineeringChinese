616997df49799b079691499e50cecfa50efea2f4
Long-Zixuan <778275146@qq.com>
2025-04-04 00:13:44 +0800

    First Commit  LZX-2025-04-04-001

---
+++ Src/PrecisionEngineering/Mod.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using ICities;
+
+namespace PrecisionEngineering
+{
+    public class Mod : IUserMod
+    {
+        public string Name
+        {
+            get { return "精确构建（Precision Engineering）"; }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return
+                    "精确构建。按住CTRL键可启用角度捕捉，按住SHIFT键可显示更多信息，按住ALT键可捕捉到参考线。";
+            }
+        }
+
+        public void OnSettingsUI(UIHelperBase helper)
+        {
+            var group = helper.AddGroup("UI");
+            group.AddDropdown("字体大小", new [] {"常规", "大", "特大"}, ModSettings.FontSize,
+                OnFontSizeChanged); 
+            
+            group.AddDropdown("计量单位", new [] {"公制", "英制"}, (int)ModSettings.Unit,
+                OnMeasurementUnitChanged);
+        }
+
+        private void OnMeasurementUnitChanged(int sel)
+        {
+            ModSettings.Unit = (ModSettings.Units) sel;
+        }
+
+        private void OnFontSizeChanged(int val)
+        {
+            ModSettings.FontSize = val;
+        }
+    }
+}
